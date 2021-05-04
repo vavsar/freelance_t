9de71e09bb2a6400e11cd159cf42e22790b6bd0d
vavsar <sarcigar@gmail.com>
2021-05-04 17:25:06 +0400

    fix fixtures

--- tests/tasks/test_views.py
+++ tests/tasks/test_views.py
@@ -17,7 +17,7 @@ EXECUTOR_EMAIL = 'executor@gmail.com'
 AUTHOR_ROLE = 'author'
 EXECUTOR_ROLE = 'executor'
 TITLE = 'test_title'
-
+TASKS_LIST_URL = reverse('tasks-list')
 
 class TaskModelTest(APITestCase):
     @classmethod
@@ -37,15 +37,16 @@ class TaskModelTest(APITestCase):
                     'status': 'active'}
         client = APIClient()
         client.force_authenticate(user=cls.author)
+        cls.TASK_DETAIL_URL = reverse('tasks-detail', args=[cls.task1.id])
 
     def test_get_tasks_list(self):
-        response = self.client.get(reverse('tasks-list'))
+        response = self.client.get(TASKS_LIST_URL)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 1)
         self.assertTrue(self.data in response.json().get('results'))
 
     def test_get_task_detail(self):
-        response = self.client.get(reverse('tasks-detail', args=[self.task1.id]))
+        response = self.client.get(self.TASK_DETAIL_URL)
         serializer_data = TasksSerializer(self.task1).data
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(serializer_data, response.data)
