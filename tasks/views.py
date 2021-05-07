5eb896d4a0ecfe6cb89e88be47687c2d869e0ccf
vavsar <sarcigar@gmail.com>
2021-05-07 19:41:23 +0400

    Feat: add two statuses for transactions improve: shorten code make code more readable

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,3 @@
-from decimal import Decimal
-
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from rest_framework import viewsets, status
@@ -14,6 +12,30 @@ from .serializers import TasksSerializer, RespondsSerializer
 User = get_user_model()
 
 
+class TransactionCreation:
+    def __init__(self, task, author, price, executor=None):
+        self.task = task
+        self.author = author
+        self.price = price
+        self.executor = executor
+
+    def create_transaction_success(self, executor=None):
+        Transaction.objects.create(
+            task=self.task,
+            author=self.task.author,
+            price=self.task.price,
+            executor=executor,
+            status='Success')
+
+    def create_transaction_fail(self, executor=None):
+        Transaction.objects.create(
+            task=self.task,
+            author=self.task.author,
+            price=self.task.price,
+            executor=executor,
+            status='Fail')
+
+
 class TasksViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TasksSerializer
@@ -25,16 +47,16 @@ class TasksViewSet(viewsets.ModelViewSet):
         self.perform_create(serializer)
         task = get_object_or_404(Task, id=serializer.data['id'])
         if task:
-            with transaction.atomic():
-                Transaction.objects.create(
-                    task=task,
-                    author=task.author,
-                    price=task.price,
-                    status='Success')
+            transact = TransactionCreation(task, task.author, task.price)
+            try:
                 user = get_object_or_404(User, id=task.author.id)
                 user.balance -= task.price
                 user.freeze_balance += task.price
                 user.save()
+                with transaction.atomic():
+                    transact.create_transaction_success()
+            except:
+                transact.create_transaction_fail()
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
@@ -63,23 +85,23 @@ class RespondViewSet(viewsets.ModelViewSet):
         task = get_object_or_404(Task, pk=self.kwargs.get('task_id'))
         respond = get_object_or_404(Respond, pk=self.kwargs.get('respond_id'))
         task_data = TasksSerializer(task).data
+        transact = TransactionCreation(task, task.author, task.price)
         if task_data['executor'] is not None:
+            transact.create_transaction_fail(task.executor)
             return Response(f'Winner is already chosen: {task.executor}')
         task_data['executor'] = respond.author.id
         task_data['status'] = 'in_progress'
-        with transaction.atomic():
-            Transaction.objects.create(
-                task=task,
-                author=task.author,
-                price=task.price,
-                executor=task.executor,
-                status='Success')
+        try:
             task_author = get_object_or_404(User, id=task.author.id)
             task_author.freeze_balance -= task.price
             task_author.save()
             task_executor = get_object_or_404(User, id=respond.author.id)
             task_executor.balance += task.price
             task_executor.save()
+            with transaction.atomic():
+                transact.create_transaction_success(respond.author)
+        except:
+            transact.create_transaction_fail(respond.author)
         serializer = TasksSerializer(task, data=task_data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
